30aa2f4eb7652507c5d267a855d6d21074613964
orcunabi21-byte <orcunabi21@gmail.com>
2026-02-01 19:49:19 +0300

    Update vcard.js

--- api/vcard.js
+++ api/vcard.js
@@ -1,1 +1,15 @@
+export default function handler(req, res) {
+  res.setHeader("Content-Type", "text/vcard; charset=utf-8");
+  res.setHeader("Content-Disposition", "inline");
+
+  res.status(200).send(`BEGIN:VCARD
+VERSION:3.0
+N:Aslan;Orçun;;;
+FN:Orçun Aslan
+TITLE:Dijital Çözüm Uzmanı
+TEL;TYPE=CELL:905413011544
+EMAIL:aslantisdesign@gmail.com
+URL:https://www.viacard.tr
+END:VCARD`);
+}
 
